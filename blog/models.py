697a9ed733d6d5ae5331eea7d822da97a901c072
pooja srinivasan <58687165+poojaseenu1999@users.noreply.github.com>
2020-08-11 19:11:15 +0530

    blogs page

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Blogs(models.Model):
-    title = models.CharField(max_length=100)
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
     description = models.TextField(max_length=250)
     date = models.DateField()
