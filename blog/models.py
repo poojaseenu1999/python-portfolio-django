b98c7d83f5f4a050c8b2852dc047cd3b96c283bd
pooja srinivasan <58687165+poojaseenu1999@users.noreply.github.com>
2020-08-12 20:05:10 +0530

    adding blogs

--- blog/models.py
+++ blog/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 class Blog(models.Model):
     title = models.CharField(max_length=200)
-    description = models.TextField(max_length=250)
+    description = models.TextField()
     date = models.DateField()
--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,8 @@
-
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
 
-    path('',views.all_blogs,name='all_blogs'),
+    path('',views.all_blogs, name='all_blogs'),
 
 ]
